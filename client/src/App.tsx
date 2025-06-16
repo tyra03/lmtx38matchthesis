6facacb857097871b9aea873f3ee37816070c9c7
tyra03 <tyra.olofsson@hotmail.se>
2025-06-16 05:08:30 +0200

    Create users

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StudentRegisterForm from "./components/studentRegistration";
+
+function App() {
+  return (
+    <div>
+      <StudentRegisterForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/chalmersPrograms.ts
@@ -0,0 +1,15 @@
+export const CHALMERS_PROGRAMS = [
+  "Datateknik",
+  "Elektroteknik",
+  "Maskinteknik",
+  "Kemiteknik",
+  "Bioteknik",
+  "Arkitektur",
+  "Industriell ekonomi",
+  "Automation och mekatronik",
+  "Väg- och vattenbyggnad",
+  "Teknisk design",
+  "Teknisk fysik",
+  "Informationsteknik",
+  // ...add more as needed
+];
